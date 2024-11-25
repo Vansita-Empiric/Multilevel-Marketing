87f77218bd2bc283faa9c4da147dad76d1d6dab9
chirag-empiric <chirag@empiricinfotech.com>
2024-11-25 11:07:37 +0530

    refactor: some changes

--- hardhat/ignition/modules/Lock.js
+++ hardhat/ignition/modules/Lock.js
@@ -1,18 +1,11 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
- 
+
 module.exports = buildModule("MLM", (m) => {
- 
+
   const token = m.contract("Token", [
     "TokenName", "SYM", 21
   ]);
- 
-  console.log("------------------>", token);
   const user = m.contract("UserContract", [
     token,
   ]);
- 
 });
- 
